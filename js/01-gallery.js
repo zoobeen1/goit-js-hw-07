5438450c639553a0629715d7fd8f7872a6eec675
Vlad Zubin <zoobeen1@gmail.com>
2022-04-22 19:27:34 +0300

    1 task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,45 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector(".gallery");
+//Создание разметки элемента галлереи
+const images = galleryItems.map((item) => {
+  const imgItem = document.createElement("div");
+  imgItem.classList.add("gallery__item");
+  const anchor = document.createElement("a");
+  anchor.classList.add("gallery__link");
+  anchor.href = item.original;
+  const img = document.createElement("img");
+  img.classList.add("gallery__image");
+  img.src = item.preview;
+  img.alt = item.description;
+  img.dataset.source = item.original;
+  anchor.append(img);
+  imgItem.append(anchor);
+  return imgItem;
+});
+// console.log(images);
+gallery.append(...images);
 
-console.log(galleryItems);
+gallery.addEventListener("click", onImgClick, false);
+
+function onImgClick(e) {
+  // отменяем стандартное действие браузера
+  e.preventDefault();
+  console.log("Target - ", e.target.dataset.source);
+  const instance = basicLightbox.create(
+    `
+	 		<img width="1400" height="900" src="${e.target.dataset.source}">
+	  	`
+  );
+  instance.show();
+}
+
+// document.querySelector("button.image").onclick = () => {
+//   basicLightbox
+//     .create(
+//       `
+// 		<img width="1400" height="900" src="https://placehold.it/1400x900">
+// 	`
+//     )
+//     .show();
+// };
